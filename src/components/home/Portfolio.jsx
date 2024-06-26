4c96a4f42fdd0e76c87b0356bd6cf3d270303743
Turgunboy Makhmudjonov <turgunboy.channel@gmail.com>
2024-06-26 18:21:56 +0500

    upg

--- src/components/home/Portfolio.jsx
+++ src/components/home/Portfolio.jsx
@@ -31,7 +31,7 @@ const data = [
     img: img4,
     title: "Netflix Clone",
     link: "https://netflix-clone-psi-olive.vercel.app",
-    githublink: "https://github.com/turgunboy01/netflix-cone",
+    githublink: "https://netflix-cone-pink.vercel.app",
   },
   {
     img: img5,
